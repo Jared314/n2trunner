ccb65f76c96368dc9a6ce8b04dfa8f60ef28f290
Jared Lobberecht <pretender314@gmail.com>
2013-02-13 22:05:09 -0600

    adding dff

---
+++ src/java/builtInGates/DFF.java
@@ -0,0 +1,19 @@
+package builtInGates;
+
+import Hack.Gates.BuiltInGate;
+import Hack.Gates.Node;
+
+public class DFF extends BuiltInGate
+{
+  private short state;
+
+  protected void clockUp()
+  {
+    this.state = this.inputPins[0].get();
+  }
+
+  protected void clockDown()
+  {
+    this.outputPins[0].set(this.state);
+  }
+}
